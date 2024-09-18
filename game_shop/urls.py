63ca5efd96112b1e240c4cdb23f8aefea8a3a792
Turin1 <XD3ay97@gmail.com>
2024-09-18 17:41:24 +0300

    Update 19.3

--- game_shop/urls.py
+++ game_shop/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from task1.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', main_task1),
+    path('main/shop/', shop_task1),
+    path('main/cart/', cart_task1),
+    path('registration/', sign_up_by_html),
 ]
--- task1/views.py
+++ task1/views.py
@@ -1,3 +1,58 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+
+
+def main_task1(request):
+    page_name = 'Главная страница'
+    context = {
+        'page_name': page_name
+    }
+    return render(request, 'main.html', context)
+
+
+def shop_task1(request):
+    page_name = 'Магазин игр'
+    back = "Вернуться назад"
+    context = {
+        'page_name': page_name,
+        'games': Game.objects.all(),
+        'back': back
+    }
+    return render(request, 'shop.html', context)
+
+
+def cart_task1(request):
+    page_name = 'Корзина'
+    text = 'Тут пока ничего нет.'
+    back = "Вернуться назад"
+    context = {
+        'page_name': page_name,
+        'text': text,
+        'back': back
+    }
+    return render(request, 'cart.html', context)
+
+
+def sign_up_by_html(request):
+    buyers = Buyer.objects.all()
+    title = 'Регистрация'
+    context = {'title': title}
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+        if password != repeat_password:
+            return HttpResponse('Пароли не совпадают')
+        elif int(age) < 18:
+            return HttpResponse('Вы должны быть старше 18 лет')
+        for buyer in buyers:
+            if username == buyer.name:
+                return HttpResponse('Пользователь уже существует')
+        Buyer.objects.create(name=username, balance=5000, age=age)
+        return HttpResponse(f'Приветствуем {username}!')
+    return render(request, 'registration_page.html', context)
