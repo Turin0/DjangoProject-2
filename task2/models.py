4e6479b9077976d1704a8ffb075cbd7af3bc2a58
Turin1 <XD3ay97@gmail.com>
2024-09-22 13:42:43 +0300

    Update 19.4

--- task2/models.py
+++ task2/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=20)
     post = models.TextField()
-    date = models.DateTimeField()
+    date = models.DateField()
 
     def __str__(self):
         return self.title
